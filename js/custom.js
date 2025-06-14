1c5d559cc3410912982e8c056bce69db37832e9a
mihajmiweb <114629869+mihajmiweb@users.noreply.github.com>
2025-06-14 13:44:05 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,629 @@
+(function($) { "use strict";
+
+
+	//Preloader
+
+	Royal_Preloader.config({
+		mode           : 'logo',
+		logo           : 'images/logo.png',
+		logo_size      : [170, 32],
+		showProgress   : true,
+		showPercentage : true,
+        text_colour: '#f8f8f8',
+		background:  '#212121'
+	});
+	
+	
+	//Navigation	
+
+	$('ul.slimmenu').on('click',function(){
+			var width = $(window).width(); 
+			if ((width <= 1200)){ 
+			$(this).slideToggle(); 
+		}	
+	});				
+	$('ul.slimmenu').slimmenu(
+			{
+			resizeWidth: '1200',
+			collapserTitle: '',
+			easingEffect:'easeInOutQuint',
+			animSpeed:'medium',
+			indentChildren: true,
+			childrenIndenter: '&raquo;'
+	});
+
+      
+	  /* Scroll animation */
+	  
+	  window.scrollReveal = new scrollReveal();
+	
+	
+	//Home text fade on scroll	
+	
+	$(window).scroll(function () { 
+        var $Fade = $('.fade-elements');
+        //Get scroll position of window 
+        var windowScroll = $(this).scrollTop();
+        //Slow scroll and fade it out 
+        $Fade.css({
+            'margin-top': -(windowScroll / 0) + "px",
+            'opacity': 1 - (windowScroll / 300)
+        });
+    });	
+	
+	/* Scroll Too */
+	
+			$(window).load(function(){"use strict";
+				
+				/* Page Scroll to id fn call */
+				$("ul.slimmenu li a,a[href='#top'],a[data-gal='m_PageScroll2id']").mPageScroll2id({
+					highlightSelector:"ul.slimmenu li a",
+					offset: 78,
+					scrollSpeed:800,
+					scrollEasing: "easeInOutCubic"
+				});
+				
+				/* demo functions */
+				$("a[rel='next']").click(function(e){
+					e.preventDefault();
+					var to=$(this).parent().parent("section").next().attr("id");
+					$.mPageScroll2id("scrollTo",to);
+				});
+				
+			});	
+
+
+
+	
+	$(document).ready(function() {
+
+		//Tooltip
+
+		$(".tipped").tipper();
+	
+		/* Separate Carousels */
+	 
+		$("#owl-sep-1").owlCarousel({
+			navigation: false,
+			pagination : true,
+			transitionStyle : "fade",
+			slideSpeed : 500,
+			paginationSpeed : 500,
+			singleItem:true,
+			autoPlay: 5000
+		});
+		
+		
+		/* Video */
+		
+		$(".container").fitVids();
+						
+		$('.vimeo a,.youtube a').click(function (e) {
+			e.preventDefault();
+			var videoLink = $(this).attr('href');
+			var classeV = $(this).parent();
+			var PlaceV = $(this).parent();
+			if ($(this).parent().hasClass('youtube')) {
+				$(this).parent().wrapAll('<div class="video-wrapper">');
+				$(PlaceV).html('<iframe frameborder="0" height="333" src="' + videoLink + '?autoplay=1&showinfo=0" title="YouTube video player" width="547"></iframe>');
+			} else {
+				$(this).parent().wrapAll('<div class="video-wrapper">');
+				$(PlaceV).html('<iframe src="' + videoLink + '?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1&amp;color=cfa144" width="500" height="281" frameborder="0"></iframe>');
+			}
+		});	
+		
+
+		/* Logos Carousel */		
+		
+		$("#owl-logos").owlCarousel({
+			items : 5,
+			itemsDesktop : [1000,4], 
+			itemsDesktopSmall : [900,3],
+			itemsTablet: [600,2], 
+			itemsMobile : false, 
+			navigation: false,
+			pagination : false,
+			autoPlay : 3000,
+			slideSpeed : 300
+		});
+
+		
+		//Facts Counter 
+	
+        $('.counter-numb').counterUp({
+            delay: 100,
+            time: 3000
+        });
+        
+        
+        //Fancybox
+		
+		$(".fancybox").fancybox({
+			maxWidth	: 1400,
+			maxHeight	: 800,
+			fitToView	: true,
+			width		: '80%',
+			height		: '80%',
+			autoSize	: false,
+			closeClick	: false,
+			openEffect	: 'elastic',
+			closeEffect	: 'none'
+		});	
+
+
+			
+ 	//Home Carousel
+		
+	  var sync1 = $("#sync1");
+	  var sync2 = $("#sync2");
+	 
+	  sync1.owlCarousel({
+		singleItem : true,
+		autoPlay: 5000,
+		transitionStyle : "fade",
+		autoHeight : false,
+		slideSpeed : 200,
+		navigation: false,
+		pagination:false,
+		afterAction : syncPosition,
+		responsiveRefreshRate : 200
+	  });
+
+	  
+	  sync2.owlCarousel({
+		items : 3,
+		itemsDesktop      : [1199,3],
+		itemsDesktopSmall     : [979,3],
+		itemsTablet       : [768,3],
+		itemsMobile       : [479,3],
+		pagination:false,
+		responsiveRefreshRate : 100,
+		afterInit : function(el){
+		  el.find(".owl-item").eq(0).addClass("synced");
+		}
+	  });
+	 
+	  function syncPosition(el){
+		var current = this.currentItem;
+		$("#sync2")
+		  .find(".owl-item")
+		  .removeClass("synced")
+		  .eq(current)
+		  .addClass("synced")
+		if($("#sync2").data("owlCarousel") !== undefined){
+		  center(current)
+		}
+	  }
+	 
+	  $("#sync2").on("click", ".owl-item", function(e){
+		e.preventDefault();
+		var number = $(this).data("owlItem");
+		sync1.trigger("owl.goTo",number);
+	  });
+	 
+	  function center(number){
+		var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
+		var num = number;
+		var found = false;
+		for(var i in sync2visible){
+		  if(num === sync2visible[i]){
+			var found = true;
+		  }
+		}
+	 
+		if(found===false){
+		  if(num>sync2visible[sync2visible.length-1]){
+			sync2.trigger("owl.goTo", num - sync2visible.length+2)
+		  }else{
+			if(num - 1 === -1){
+			  num = 0;
+			}
+			sync2.trigger("owl.goTo", num);
+		  }
+		} else if(num === sync2visible[sync2visible.length-1]){
+		  sync2.trigger("owl.goTo", sync2visible[1])
+		} else if(num === sync2visible[0]){
+		  sync2.trigger("owl.goTo", num-1)
+		}
+		
+	  }
+
+
+		
+		
+	//Set your google maps parameters
+
+		var latitude = 44.805574,
+			longitude = 20.464323,
+			map_zoom = 17;
+
+		//google map custom marker icon - .png fallback for IE11
+		var is_internetExplorer11= navigator.userAgent.toLowerCase().indexOf('trident') > -1;
+		var marker_url = ( is_internetExplorer11 ) ? 'images/cd-icon-location.png' : 'images/cd-icon-location.svg';
+			
+		//define the basic color of your map, plus a value for saturation and brightness
+		var	main_color = '#e67e22',
+			saturation_value= -50,
+			brightness_value= 14;
+
+		//we define here the style of the map
+		var style= [
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    }
+];
+			
+		//set google map options
+		var map_options = {
+			center: new google.maps.LatLng(latitude, longitude),
+			zoom: map_zoom,
+			panControl: true,
+			zoomControl: true,
+			mapTypeControl: false,
+			streetViewControl: false,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			scrollwheel: false,
+			styles: style,
+		}
+		//inizialize the map
+		var map = new google.maps.Map(document.getElementById('google-container'), map_options);
+		//add a custom marker to the map				
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(latitude, longitude),
+			map: map,
+			visible: true,
+			icon: marker_url,
+		});
+
+		//add custom buttons for the zoom-in/zoom-out on the map
+		function CustomZoomControl(controlDiv, map) {
+			//grap the zoom elements from the DOM and insert them in the map 
+			var controlUIzoomIn= document.getElementById('cd-zoom-in'),
+				controlUIzoomOut= document.getElementById('cd-zoom-out');
+			controlDiv.appendChild(controlUIzoomIn);
+			controlDiv.appendChild(controlUIzoomOut);
+
+			// Setup the click event listeners and zoom-in or out according to the clicked element
+			google.maps.event.addDomListener(controlUIzoomIn, 'click', function() {
+				map.setZoom(map.getZoom()+1)
+			});
+			google.maps.event.addDomListener(controlUIzoomOut, 'click', function() {
+				map.setZoom(map.getZoom()-1)
+			});
+		}
+
+		var zoomControlDiv = document.createElement('div');
+		var zoomControl = new CustomZoomControl(zoomControlDiv, map);
+
+		//insert the zoom div on the top left of the map
+		map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
+
+
+
+		
+		
+	/* Portfolio Sorting */
+
+		(function ($) { 
+		
+		
+			var container = $('#projects-grid');
+			
+			
+			function getNumbColumns() { 
+				var winWidth = $(window).width(), 
+					columnNumb = 1;
+				
+				
+				if (winWidth > 1500) {
+					columnNumb = 4;
+				} else if (winWidth > 1200) {
+					columnNumb = 3;
+				} else if (winWidth > 900) {
+					columnNumb = 2;
+				} else if (winWidth > 600) {
+					columnNumb = 2;
+				} else if (winWidth > 300) {
+					columnNumb = 1;
+				}
+				
+				return columnNumb;
+			}
+			
+			
+			function setColumnWidth() { 
+				var winWidth = $(window).width(), 
+					columnNumb = getNumbColumns(), 
+					postWidth = Math.floor(winWidth / columnNumb);
+
+			}
+			
+			$('#portfolio-filter #filter a').click(function () { 
+				var selector = $(this).attr('data-filter');
+				
+				$(this).parent().parent().find('a').removeClass('current');
+				$(this).addClass('current');
+				
+				container.isotope( { 
+					filter : selector 
+				});
+				
+				setTimeout(function () { 
+					reArrangeProjects();
+				}, 300);
+				
+				
+				return false;
+			});
+			
+			function reArrangeProjects() { 
+				setColumnWidth();
+				container.isotope('reLayout');
+			}
+			
+			
+			container.imagesLoaded(function () { 
+				setColumnWidth();
+				
+				
+				container.isotope( { 
+					itemSelector : '.portfolio-box-1', 
+					layoutMode : 'masonry', 
+					resizable : false 
+				} );
+			} );
+			
+			
+		
+			
+		
+			$(window).on('debouncedresize', function () { 
+				reArrangeProjects();
+				
+			} );
+			
+		
+		} )(jQuery);
+
+
+		
+	});	
+
+ 
+ 
+	/* DebouncedResize Function */
+		(function ($) { 
+			var $event = $.event, 
+				$special, 
+				resizeTimeout;
+			
+			
+			$special = $event.special.debouncedresize = { 
+				setup : function () { 
+					$(this).on('resize', $special.handler);
+				}, 
+				teardown : function () { 
+					$(this).off('resize', $special.handler);
+				}, 
+				handler : function (event, execAsap) { 
+					var context = this, 
+						args = arguments, 
+						dispatch = function () { 
+							event.type = 'debouncedresize';
+							
+							$event.dispatch.apply(context, args);
+						};
+					
+					
+					if (resizeTimeout) {
+						clearTimeout(resizeTimeout);
+					}
+					
+					
+					execAsap ? dispatch() : resizeTimeout = setTimeout(dispatch, $special.threshold);
+				}, 
+				threshold : 150 
+			};
+		} )(jQuery);
+
+
+
+	
+	
+  })(jQuery); 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+	
